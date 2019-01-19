11b1b34ee68a931efcc83f7cc5001728a8a7192a
JerYoMat <jeremy.emata@gmail.com>
2019-01-19 18:19:54 -0500

    added index action to users controller, added styling for index view, updated link in header partial to go to index view, added index.html.erb for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index,:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index 
+    @title = "All Users"
+    @users = User.all 
+  end 
 
 
   def new 
