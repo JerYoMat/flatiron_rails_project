c717e2e40499085b35e699ad686b00ab22d5027c
JerYoMat <jeremy.emata@gmail.com>
2019-01-19 17:59:36 -0500

    added edit and update to users, implemented partial for new/edit of users, updated user model to allow password to equal nil when updating an existing user, included edit link in header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
-  def show
-    @title = "Profile Page"
-    @user = User.find(params[:id])
-    #debugger #uses byebug gem and provides a console in the browser
-  end 
+  
 
   def new 
     @title = "Sign Up"
@@ -19,10 +15,37 @@ class UsersController < ApplicationController
       render 'new' 
     end 
   end 
+
+  def show
+    @title = "Profile Page"
+    set_user
+    #debugger #uses byebug gem and provides a console in the browser
+  end 
+  
+  def edit 
+    @title = "Edit User Info"
+    set_user
+  end 
+
+  def update 
+    set_user
+    if @user.update_attributes(user_params)
+      flash[:success] = "You changes have been saved."
+      redirect_to @user
+    else 
+      render 'edit'
+    end 
+  end 
+
+
 private 
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
+
+  def set_user 
+    @user = User.find(params[:id])
   end 
 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ validates :email, presence: true, length: { maximum: 255 },
                     format: { with: EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 has_secure_password
-validates :password, presence: true, length: { minimum: 6 }
+validates :password, presence: true, length: { minimum: 6 },  allow_nil: true #the nil is there so when a user updates their profile they do not have to enter their password again.  New users are still required to provide a password with at least six characters. 
 
 def self.digest(string)
   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
