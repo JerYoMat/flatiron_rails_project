69d5f89e9ca007ce89c524c8f401e39718e855e6
JerYoMat <jeremy.emata@gmail.com>
2019-01-19 18:11:11 -0500

    updated sessions_helper with new methods to enable friendly-forwarding and implemented in sessions controller, added login controls in UsersController.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,6 @@ module SessionsHelper
         cookies.delete(:remember_token)
       end
 
-
       def current_user
         if (user_id = session[:user_id])
           @current_user ||= User.find_by(id: user_id)
@@ -27,8 +26,7 @@ module SessionsHelper
           end
         end
       end
-    
-      
+          
       def logged_in?
         !current_user.nil?
       end
@@ -37,6 +35,20 @@ module SessionsHelper
         forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+      end
+
+      def current_user?(user)
+        user == current_user
+      end
+
+#The below two methods are there to enable 'friendly-forwarding'.  If a user is prompted to log in when trying to access a page that requires login, then after providing credentials, the user experience is improved if the user is redirected to the original site they were trying to access.  
+      def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+      end
+
+      def store_location
+        session[:forwarding_url] = request.original_url if request.get?
       end
 
     end
