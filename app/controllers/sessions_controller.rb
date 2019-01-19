ab35c74e557f9a9aec055d001ba8dee96b172d7d
JerYoMat <jeremy.emata@gmail.com>
2019-01-19 17:35:04 -0500

    added ability for a user to remember their credentials by updating sessions_controller, helper, user model and updating schema.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
      
         log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_to user
      
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
 
 validates :name, presence: true, length: {maximum:50} 
-before_save { email.downcase! }
+before_save { self.email = email.downcase }
 EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 255 },
                     format: { with: EMAIL_REGEX },
@@ -9,21 +10,31 @@ validates :email, presence: true, length: { maximum: 255 },
 has_secure_password
 validates :password, presence: true, length: { minimum: 6 }
 
-def create 
-  end 
+def self.digest(string)
+  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                BCrypt::Engine.cost
+  BCrypt::Password.create(string, cost: cost)
+end
 
-  def edit
-  end 
+# Returns a random token with very low probability of duplication
+def self.new_token
+  SecureRandom.urlsafe_base64
+end
 
-  def show 
-  end 
-
-  def update 
-  end 
-
-  def delete
-  end 
+def remember
+  self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+end
 
 
+#Note that the remember_token argument in the authenticated? method is not the same as the attr_accessor defined up top.  
+def authenticated?(remember_token)
+  return false if remember_digest.nil?  
+  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+end
+
+def forget
+  update_attribute(:remember_digest, nil)
+end
 
 end
