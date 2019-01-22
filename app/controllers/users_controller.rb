bc707c26cb5207f9f1ea3daf949d1c8f5f6e8910
JerYoMat <jeremy.emata@gmail.com>
2019-01-22 16:48:08 -0500

    updates users create action to log in newly created users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save 
       flash[:success] = "Welcome, a user was successfully created!"
+      log_in @user 
       redirect_to @user 
     else
       render 'new' 
